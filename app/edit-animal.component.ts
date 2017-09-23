07db0f4975a00431a6fb1042e88ba89584a30aa6
Paul Guevarra <p.a.guevarra@gmail.com>
2017-09-22 22:34:23 -0700

    create editanimalcomponent to manage edit Animal objects

---
+++ app/edit-animal.component.ts
@@ -0,0 +1,53 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Animal } from './animal.model';
+
+@Component({
+  selector: 'edit-animal',
+  template: `
+  <div>
+    <div *ngIf="childSelectedAnimal">
+    <hr>
+      <h3>{{childSelectedAnimal.species}}</h3>
+      <p>Task Complete? {{childSelectedAnimal.done}}</p>
+      <h3>Edit Animal</h3>
+      <label>Enter Animal Species:</label>
+      <input [(ngModel)]="childSelectedAnimal.species">
+      <br>
+      <label>Enter Animal Name:</label>
+      <input [(ngModel)]="childSelectedAnimal.name">
+      <br>
+      <label>Enter Animal Age:</label>
+      <input [(ngModel)]="childSelectedAnimal.age">
+      <br>
+      <label>Enter Animal Diet:</label>
+      <input [(ngModel)]="childSelectedAnimal.diet">
+      <br>
+      <label>Enter Animal Location:</label>
+      <input [(ngModel)]="childSelectedAnimal.location">
+      <br>
+      <label>Enter Animal Caretakers:</label>
+      <input [(ngModel)]="childSelectedAnimal.caretakers">
+      <br>
+      <label>Enter Animal Sex:</label>
+      <input [(ngModel)]="childSelectedAnimal.sex">
+      <br>
+      <label>Enter Animal Likes:</label>
+      <input [(ngModel)]="childSelectedAnimal.likes">
+      <br>
+      <label>Enter Animal Dislikes:</label>
+      <input [(ngModel)]="childSelectedAnimal.dislikes">
+      <br>
+      <button (click)="doneButtonClicked()">Done</button>
+    </div>
+  </div>
+  `
+})
+
+export class EditAnimalComponent {
+  @Input() childSelectedAnimal: Animal;
+  @Output() doneButtonClickedSender = new EventEmitter();
+
+  doneButtonClicked() {
+    this.doneButtonClickedSender.emit();
+  }
+}
